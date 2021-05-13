10cf600f6d9acf4a65a2d7514f04e8fd8280aa59
Alexei Pugachev <lenyapugachev@me.com>
2021-05-14 01:48:19 +0700

    Change interpreter

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
+      interpreter : 'node@11.15.0',
       max_memory_restart: '1G',
       env_production: {
         NODE_ENV: 'production',
@@ -19,6 +20,7 @@ module.exports = {
       host : 'gb.lenyapugachev.com',
       key  : '~/.ssh/id_rsa',
       ref  : 'origin/master',
+
       repo : 'git@github.com:alek-sai/minqbot.git',
       path : '/var/www/minqbot',
       'post-deploy' : 'npm i && ./node_modules/pm2/bin/pm2 start ecosystem.config.js --env production'
