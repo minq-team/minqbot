30b3eacbde39b0884e92cde2d93d36c0f536d805
nasushkov <nasushkov@gmail.com>
2021-05-16 18:59:43 +0300

    fix(linter fixes): fixed some code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,29 +1,30 @@
 module.exports = {
   apps: [
     {
-      name: 'MinqBot',
-      script: 'index.js',
+      name: "MinqBot",
+      script: "index.js",
       instances: 1,
       autorestart: true,
       watch: false,
-      interpreter : 'node@11.15.0',
-      max_memory_restart: '1G',
+      interpreter: "node@11.15.0",
+      max_memory_restart: "1G",
       env_production: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
       },
     },
   ],
 
-  deploy : {
-    production : {
-      user : 'root',
-      host : 'gb.lenyapugachev.com',
-      key  : '~/.ssh/id_rsa',
-      ref  : 'origin/master',
+  deploy: {
+    production: {
+      user: "root",
+      host: "gb.lenyapugachev.com",
+      key: "~/.ssh/id_rsa",
+      ref: "origin/master",
 
-      repo : 'git@github.com:alek-sai/minqbot.git',
-      path : '/var/www/minqbot',
-      'post-deploy' : 'npm i && ./node_modules/pm2/bin/pm2 start ecosystem.config.js --env production'
-    }
-  }
+      repo: "git@github.com:alek-sai/minqbot.git",
+      path: "/var/www/minqbot",
+      "post-deploy":
+        "npm i && ./node_modules/pm2/bin/pm2 start ecosystem.config.js --env production",
+    },
+  },
 };
