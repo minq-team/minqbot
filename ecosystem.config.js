0f39f0115f090093d6e40bd3e7ff11bc7babe250
Alexei Pugachev <lenyapugachev@me.com>
2021-05-18 21:51:14 +0700

    RU

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,9 @@ module.exports = {
       env_production: {
         NODE_ENV: 'production',
       },
+      env_production_ru: {
+        NODE_ENV: 'production',
+      },
     },
   ],
 
@@ -22,6 +25,16 @@ module.exports = {
 
       repo : 'git@github.com:alek-sai/minqbot.git',
       path : '/var/www/minqbot',
+      'post-deploy' : 'npm i && ./node_modules/pm2/bin/pm2 start ecosystem.config.js --env production'
+    },
+    production_ru : {
+      user : 'root',
+      host : 'gb.lenyapugachev.com',
+      key  : '~/.ssh/id_rsa',
+      ref  : 'origin/master',
+
+      repo : 'git@github.com:alek-sai/minqbot.git',
+      path : '/var/www/minqbotru',
       'post-deploy' : 'npm i && ./node_modules/pm2/bin/pm2 start ecosystem.config.js --env production'
     }
   }
